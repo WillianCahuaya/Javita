908db93a458ac5c982bb5195e0fcc5947ae26f1f
Jhonantaurco96 <jhon_05_1996@hotmail.com>
2025-02-17 19:45:37 -0500

    actualizando netbens category y postman

--- BACKEND/autonoma/src/main/java/com/autonoma/controllers/CategoryController.java
+++ BACKEND/autonoma/src/main/java/com/autonoma/controllers/CategoryController.java
@@ -1,10 +1,14 @@
 package com.autonoma.controllers;
 
+import com.autonoma.dtos.CategoryDto;
+import com.autonoma.utils.MemoryCategory;
+import java.util.Map;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,23 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     @PostMapping("create")
-    public String createCategories() {
-        return "Se ha creado categorias";
+    public String createCategory(@RequestBody CategoryDto categoryDto) {
+        MemoryCategory memoryCategory = new MemoryCategory();
+        memoryCategory.create(categoryDto);
+        return "categoria creado: " + categoryDto;
     }
-    
-    @GetMapping("read/{id}")
-    public String getCategories(@PathVariable("id") Integer id) {
-        return "Obteniendo mi usuario con nombre: ";
+
+    @GetMapping("get/{id}")
+    public String getCategoriesById(@PathVariable("id") Integer id) {
+        MemoryCategory memoryCategory = new MemoryCategory();
+        String value = memoryCategory.getById(id);
+        return "categoria:  " + value;
     }
-    
+
+    @GetMapping("getAll")
+    public Map getAllCategories() {
+        MemoryCategory memoryCategory = new MemoryCategory();
+        return memoryCategory.getAll();
+    }
+
     @PutMapping("update")
-    public String updateCategories() {
-        return "Actualziando mi usuario";
+    public String updateCategory() {
+        return "Actualziando categorias";
     }
-    
+
     @DeleteMapping("delete")
-    public String deleteCategories() {
-        return "Eliminando mi usuario";
+    public String deleteCategory() {
+        return "Eliminando categorias";
     }
-   
-} 
+
+}
--- BACKEND/autonoma/src/main/java/com/autonoma/utils/MemoryCategory.java
+++ BACKEND/autonoma/src/main/java/com/autonoma/utils/MemoryCategory.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.autonoma.utils;
 
-/**
- *
- * @author jhon
- */
+import com.autonoma.dtos.CategoryDto;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MemoryCategory {
     
+    static Map memory = null;
+    
+    public MemoryCategory(){
+        init();
+    }
+    
+    private void init() {
+        if (memory == null) {
+            System.out.println("construyendo categorias");
+            memory = new HashMap<Integer, String>();
+            memory.put(1, "Correo");
+            memory.put(2, "Telefono");
+            memory.put(3, "Direccion");
+        }
+    }
+    
+    public String getById(int id) {
+        String value = (String) memory.get(id);
+        return value;
+    }
+    
+    public Map getAll(){
+        return memory;
+    }
+    
+    public void create (CategoryDto categoryDto){
+        memory.put(categoryDto.getId(), categoryDto.getName());
+    }
 }
