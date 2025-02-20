3642a1c4ec327aee362e573a15123c65e95f1855
Jhonantaurco96 <jhon_05_1996@hotmail.com>
2025-02-20 14:27:37 -0500

    declarar dos variables numericas division y multiplicacion

---
+++ BACKEND/autonoma/src/main/java/ejercicios/multiplicaciondivision.java
@@ -0,0 +1,14 @@
+
+package ejercicios;
+
+
+public class multiplicaciondivision {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 15;
+        int multiplicacion = num1*num2;
+        int division = num2/num1;
+        System.out.println("La multiplicacion de " + num1 + " * " + num2 + " es: " + multiplicacion);
+        System.out.println("La division de " + num2 + " / " + num1 + " es: " + division);
+    }
+}
