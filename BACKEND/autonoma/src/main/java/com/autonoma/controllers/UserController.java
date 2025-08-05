3a1dec0b7718dcf3e20385c77b37d6553f777c1b
WillianCahuaya <wcahuayaramos@gmail.com>
2025-08-04 23:11:25 -0500

    Actualizando base de datos - users

--- BACKEND/autonoma/src/main/java/com/autonoma/controllers/UserController.java
+++ BACKEND/autonoma/src/main/java/com/autonoma/controllers/UserController.java
@@ -1,17 +1,10 @@
 package com.autonoma.controllers;
 
-import com.autonoma.utils.MemoryUser;
 import com.autonoma.dtos.UserDto;
+import com.autonoma.utils.MemoryUser;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Map;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
@@ -21,7 +14,7 @@ public class UserController {
     
     @PostMapping("create")
     public String createUser(@RequestBody UserDto userDto) {
-        MemoryUser memoryUser = new MemoryUser(); //Para crear una instancia en un objeto
+        MemoryUser memoryUser = new MemoryUser();
         memoryUser.create(userDto);
         return "Usuario creado: " + userDto;
     }
--- BACKEND/autonoma/src/main/java/com/autonoma/utils/MemoryUser.java
+++ BACKEND/autonoma/src/main/java/com/autonoma/utils/MemoryUser.java
@@ -1,16 +1,15 @@
 package com.autonoma.utils;
 
 import com.autonoma.dtos.UserDto;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 //clase
 public class MemoryUser {
 
     //declaracion de variable a nivel de clase
-    static Map memory = null;
+    static List<UserDto> memoryList = null;
 
     //constructor
     public MemoryUser() {
@@ -19,32 +18,28 @@ public class MemoryUser {
 
     //metodo
     private void init() {
-        if (memory == null) {
-            System.out.println("Contruyendo usuarios");
-            memory = new HashMap<Integer, UserDto>();
-            memory.put(1, new UserDto(1, "Pedro"));
-            memory.put(2, new UserDto(2, "Juan"));
-            memory.put(3, new UserDto(3, "Miguel"));
-            memory.put(4, new UserDto(4, "Jose"));
-            memory.put(5, new UserDto(5, "Ernesto"));
-            memory.put(7, new UserDto(7, "Gabriel"));
-            memory.put(6, new UserDto(6, "Luis"));
+        int i = 0;
+        if (memoryList == null) {
+            memoryList = new ArrayList<UserDto>();
+            memoryList.add(new UserDto(++i, "Pedro"));
+            memoryList.add(new UserDto(++i, "Juan"));
+            memoryList.add(new UserDto(++i, "Miguel"));
+            memoryList.add(new UserDto(++i, "Jose"));
         }
     }
 
     //metodo
     public UserDto getById(int id) {
-        return (UserDto) memory.get(id);
+        return (UserDto) memoryList.get(id);
     }
     
     public List getAll() {
-        List<UserDto> list = new ArrayList<>(memory.values());
-        return list;
+        return memoryList;
     }
     
     //metodo
     public void create(UserDto userDto) {
-        memory.put(userDto.getId(), userDto.getName());
+        memoryList.add(userDto);
     }
 
 }
