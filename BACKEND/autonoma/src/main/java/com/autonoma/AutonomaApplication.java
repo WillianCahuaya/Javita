c14c58737576934aa897a120387d666d706d7beb
WillianCahuaya <wcahuayaramos@gmail.com>
2025-08-11 23:45:22 -0500

    Subiendo conexion java con MariaDB

--- BACKEND/autonoma/src/main/java/com/autonoma/AutonomaApplication.java
+++ BACKEND/autonoma/src/main/java/com/autonoma/AutonomaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutonomaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutonomaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutonomaApplication.class, args);
+    }
 
 }
---
+++ BACKEND/autonoma/src/main/java/com/autonoma/entities/UserEntity.java
@@ -0,0 +1,87 @@
+package com.autonoma.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String dni;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String dni, String username, String email, String role, String password) {
+        this.dni = dni;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
